8f4d0102d9e87249e406dfc7df90b062f0dca2ac
MrBoomDev <github@mrboomdev.ru>
2023-10-17 21:44:31 +0500

    Individual games for all chats, Game state

--- commands/mafia.ts
+++ commands/mafia.ts
@@ -1,37 +1,66 @@
-import { addCommand } from "..";
+import { addCommand, bot } from "..";
 import { reply } from "../util/reply";
 
-const game = {
-	isStarted: false
+const games: Record<number, Game | null> = {};
+
+interface Game {
+	isStarted: boolean,
+	startedTime: number,
+	interval: NodeJS.Timeout
+}
+
+function updateMafia(chat: number) {
+	bot.sendMessage(chat, "update");
 }
 
 export function initMafiaCommands() {
 	addCommand("mafia", message => {
-		if(game.isStarted) {
-			reply(message, "Зачем начинать вторую игру? Будто-бы в первую много кто играет...");
+		const game = games[message.chat.id];
+
+		if(game == null) {
+			games[message.chat.id] = {
+				isStarted: false,
+				startedTime: Date.now(),
+				interval: setInterval(() => updateMafia(message.chat.id), 1_000)
+			}
+
+			reply(message, "Ну начинаем же набор в игру \"Мафия\"!");
 			return;
 		}
 
-		game.isStarted = true;
-		reply(message, "Ну начинаем же набор в игру \"Мафия\"!");
+		reply(message, "Подожди пока первая игра закончиться, а то тут будет полный бардак!");
 	});
 
 	addCommand("mafia_stop", message => {
-		if(!game.isStarted) {
+		const game = games[message.chat.id];
+
+		if(game == null) {
 			reply(message, "Но игра даже не начиналась...");
 			return;
 		}
 		
-		game.isStarted = false;
 		reply(message, "Похоже, что нам придется отменить эту игру :(");
+		games[message.chat.id] = null;
+
+		if(game.interval != null) {
+			clearInterval(game.interval);
+		}
 	});
 
 	addCommand("mafia_extend", message => {
-		if(!game.isStarted) {
+		const game = games[message.chat.id];
+
+		if(game == null) {
 			reply(message, "Как увеличить то, чего нет...");
 			return;
 		}
 
+		if(game.isStarted) {
+			reply(message, "Игра уже началась! Тебе придется отменить ее при помощи /mafia_stop");
+			return;
+		}
+
+		game.startedTime += 30_000;
 		reply(message, "Хорошо, даю вам еще +30 секунд!");
 	});
 }
